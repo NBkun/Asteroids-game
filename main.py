b8c0c8a348630721d91beb76ca85b9e58427d45f
NBkun <nguyennbkun@gmail.com>
2025-05-15 14:57:49 +0700

    made asteroids splits or disappear when shot

--- main.py
+++ main.py
@@ -36,6 +36,10 @@ def main():
             if asteroid.collision(player):
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if shot.collision(asteroid):
+                    asteroid.split()
+                    shot.kill()
 
         screen.fill("black")
 
